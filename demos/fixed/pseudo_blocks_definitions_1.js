2fa3cf5d89c03366129a70542781268a449db301
Josiah <spacetagman@gmail.com>
2017-10-16 18:46:27 -0500

    Initial pseudocode blocks and other code (from Blockly Developer Tools)

---
+++ demos/fixed/pseudo_blocks_definitions_1.js
@@ -0,0 +1,22 @@
+Blockly.Blocks['pseudo_statement'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldTextInput("pseudocode"), "NAME");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(300);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['pseudo_expression'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldTextInput("pseudo-expression"), "NAME");
+    this.setOutput(true, null);
+    this.setColour(300);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
